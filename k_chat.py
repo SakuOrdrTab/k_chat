2cf9dbc88a8a666c910e7c240f3dcbbbcfe05091
SakuOrdrTab <leinosensaku@gmail.com>
2024-03-10 17:28:31 +0200

    changed gui to be defined in kivy file. Also phi2api put to src folder

--- k_chat.py
+++ k_chat.py
@@ -8,31 +8,20 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 
-from phi2_api import Phi2_api
+from src.phi2_api import Phi2_api
+
+# Assuming you will use this later
+# from phi2_api import Phi2_api
 
 class MainWindow(BoxLayout):
-    def __init__(self, **kwargs):
-        super(MainWindow, self).__init__(**kwargs)
-        self.orientation = 'vertical'
-
-        self.prompt_textinput = TextInput(size_hint_y=0.15,
-                                          font_size=15,
-                                          multiline=False)
-        self.output_label = Label(text="This will have text",
-                       font_size=15)
-        self.prompt_textinput.bind(on_text_validate=self.on_prompt_textinput_enter)
-        self.add_widget(self.prompt_textinput)
-        self.add_widget(self.output_label)
-
-    def on_prompt_textinput_enter(self, instance):
-        self.output_label.text = instance.text.upper()
+    def prompt_entered(self, instance):
+        self.ids.output_label.text = instance.text.upper()
 
 class ChatApp(App):
     def build(self):
         return MainWindow()
 
-
 if __name__=="__main__":
     print(kivy.__version__)
-    phi2 = Phi2_api()
+    phi2 = Phi2_api()  # Assuming this is for future use
     ChatApp().run()
