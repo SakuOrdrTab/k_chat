32328248539061154858846e01e26eb558f41213
SakuOrdrTab <leinosensaku@gmail.com>
2024-03-12 20:50:32 +0200

    Added chatgpt 3.5 turbo API

--- chatgpt35_api.py
+++ chatgpt35_api.py
@@ -1,24 +1,44 @@
 import context_memory
-import openai
+from openai import OpenAI
 import os
 from colorama import Fore, Style
 
 class Chatgpt35_api():
     def __init__(self):
         self._context_memory = context_memory.Context_memory(max_size=10000)
-        self.api_key = os.getenv("OPENAI_API_KEY")
-        if self.api_key is None:
-            raise ValueError("OpenAI API key not found")
+        # Seems like the 3.5 turbo does not require api key anymore?
+        # self.api_key = os.getenv("OPENAI_API_KEY")
+        # if self.api_key is None:
+        #     raise ValueError("OpenAI API key not found")
+        
         self._model = "gpt-3.5-turbo"
+        self._client = OpenAI()
+
+
+    def wrap_prompt(self, prompt : str, include_context_memory : bool = True) -> str:
+        '''Just add the context memory'''
+        final_prompt = ""
+        if include_context_memory:
+            final_prompt += "Chat history:\n" + self._context_memory.get_context_memory() + "\n"
+        
+        final_prompt += prompt + "\n"
+        return final_prompt
 
     def get_answer(self, prompt : str, raw_answer : bool = False) -> str:
-        response = openai.Completion.create(
-            engine=self._model,
-            prompt=prompt,
-            n=1,
-            stop=None
+        response = self._client.chat.completions.create(
+            model=self._model,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a chat bot."
+                },
+                {
+                    "role": "user",
+                    "content": self.wrap_prompt(prompt)
+                }
+            ]
         )
-        answer = response.choices[0].text.strip()
+        answer = response.choices[0].message.content
         self._context_memory.append_chat_lines(user_str=prompt, answer_str=answer)
         return answer
 
